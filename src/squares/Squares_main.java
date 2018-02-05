431eb711513f5ab2d811393693af3f88af48e93d
50616032 <50616032@50616032-pc>
2018-02-05 09:16:44 +0900

    50616032

--- src/squares/Squares_main.java
+++ src/squares/Squares_main.java
@@ -1,9 +1,12 @@
 package squares;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 
 public class Squares_main {
@@ -19,17 +22,26 @@ public class Squares_main {
 
 		Squares_lib slib = new Squares_lib(x, y);
 		double value[] = new double[100];
+		double value2[][] = new double[100][4];
+		
 		for(int i = 0; i < 100; i++){
             theta = slib.getDx();
             System.out.println(i+" : theta[0] = "+theta[0]);
             System.out.println(i+" : theta[1] = "+theta[1]);
             System.out.println(i+" : Objective function = "+slib.getObject());
             value[i] = slib.getObject();
+            value2[i][0] = i;
+            value2[i][1] = slib.getObject();
+            value2[i][2] = theta[0];
+            value2[i][3] = theta[1];
+            
         }
 		
 		Graph graph = new Graph(value);
 		graph.setBounds(5,5,655,455);
         graph.setVisible(true);
+        
+        smain.writeCSV("CSV/risult.CSV", value2);
         
 	}//main関数終了
 	
@@ -59,6 +71,37 @@ public class Squares_main {
         return csvdata;
         //CSVから取り込みここまで
     }
+    
+    public void writeCSV(String path, double[][] data) {
+    	try {
+			FileWriter fw = new FileWriter(path, true); //true:追記、false:上書き
+			PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+			//内容を指定する
+			for(int i = 0; i < data.length; i++) {
+				
+				for(int j = 0; j < data[0].length; j++) {
+					pw.print(data[i][j]);
+					pw.print(",");
+				}
+				
+				
+			    //pw.print(",");
+			    pw.println();
+			}
+			pw.println();
+			
+			//ファイルに書き出す
+			pw.close();
+
+			//終了メッセージを画面に出力する
+			System.out.println("出力完了 : "+path);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+    }
+    
 
 
 }
